0e5647d123edb746b6f8a7abc93bd848834d53ae
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2019-05-27 23:09:49 +0300

    Tehtävät 3.9 - 3.11 (Heroku deploy)

--- osa3/puhelinluettelo-backend/server.js
+++ osa3/puhelinluettelo-backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("body", function(req, res) {
   if (req.method === "POST") return JSON.stringify(req.body);
 });
 
+app.use(express.static("build"));
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] :response-time ms :body")
 );
