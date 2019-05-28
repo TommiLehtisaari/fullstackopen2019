9348f35aa6914c70a24862991fb404701c3d6105
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2019-05-28 17:19:14 +0300

    Tehtävä 3.12 (MongoDB Atlas)

---
+++ osa3/mongodb-tietokanta/index.js
@@ -0,0 +1,40 @@
+const config = require("config");
+const mongoose = require("mongoose");
+
+if (process.argv.length !== 5 && process.argv.length !== 3) {
+  console.log("Esim. node index.js salasana nimi numero");
+  process.exit(1);
+}
+
+mongoose.connect(config.get("mongo_url"), { useNewUrlParser: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log("Puhelinluettelo:");
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  const person = new Person({
+    name,
+    number
+  });
+
+  person.save().then(result => {
+    console.log(`lisätään ${name} numero ${number} luetteloon`);
+    mongoose.connection.close();
+  });
+}
